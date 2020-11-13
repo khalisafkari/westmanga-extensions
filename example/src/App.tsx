1e9491deaa3e24ec590359de4955942239c94941
Khalis Afkari <khalis411@gmail.com>
2020-11-14 01:36:51 +0700

    fix

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
-import { Home } from 'westmanga-extensions';
+import API, { Home } from 'westmanga-extensions';
 
 export default function App() {
   React.useEffect(() => {
-    Home({}).then(console.log);
+    Home({})
+      .then(console.log)
+      .catch((e) => {
+        console.log('error');
+        console.log(e);
+      });
+    API().then(console.log).catch(console.log);
   }, []);
 
   return (
